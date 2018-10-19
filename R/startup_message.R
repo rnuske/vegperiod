8af7bd9129c441598eb292dcfe82f1b4ec6a1b47
Robert Nuske <robert.nuske@nw-fva.de>
2018-10-19 11:19:10 +0200

    done away with startup message

--- R/startup_message.R
+++
@@ -1,7 +0,0 @@
-# startup message
-.onAttach <- function(libname, pkgname) {
-  packageStartupMessage(
-    paste("Note: Reading 'historical' data with",
-          "read.DWDdata() requires the package 'curl'.")
-    )
-}
